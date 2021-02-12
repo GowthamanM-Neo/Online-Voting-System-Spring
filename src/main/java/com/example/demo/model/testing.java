4bd2e2378cba17fe64ec42de4a8593f77cdcc8d7
GowthamanM-Neo <74287219+GowthamanM-Neo@users.noreply.github.com>
2021-02-12 17:39:43 +0530

    testing Doa added

---
+++ src/main/java/com/example/demo/model/testing.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class testing {
+
+	@Id
+	private String id;
+	private String testingName;
+	
+}
